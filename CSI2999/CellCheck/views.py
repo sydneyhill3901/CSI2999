36ab48dbeca2f18c681771605d15befc113d21b7
Mike <mooremk95@gmail.com>
2020-03-03 13:48:10 -0500

    Specified Contexts for Index and Manufacturer Views
    Contexts were defined for the index and manufacturer views. Comments were added to describe what type of data each variable will hold. This information can be used to implement django template rendering for these pages.

--- CSI2999/CellCheck/views.py
+++ CSI2999/CellCheck/views.py
@@ -6,15 +6,49 @@ from django.template import loader
 # Create your views here.
 
 def index(request):	
-	context = {}
+	# Manufacturer names as strings, associated links to phone images as well. 
+	context = {
+				"Manufacturer1":str(),
+				"Manufacturer2":str(),
+				"Manufacturer3":str(),
+				"Manufacturer4":str(),
+				"phone1URL":str(),
+				"phone2URL":str(),
+				"phone3URL":str(),
+				"phone4URL":str(),
+				}	
 	template = loader.get_template("CellCheck/index.html")
 	return HttpResponse(template.render(context,request)) #render(request, "CellCheck/index.html", context)
 
 def Manufacturer(request):
-	context = {}
+	# Phone# phone names, phone#URL urls to images, phoneList: list of strings containing the remaining phones from the manufacturer
+	context = {
+				"phone1":str(),
+				"phone1URL":str(),
+				"phone2":str(),
+				"phone2URL":str(),
+				"phone3":str(),
+				"phone3URL":str(),
+				"phone4":str(),
+				"phone4URL":str(),
+				"phoneList":[],
+				}
 
 	return render(request, "CellCheck/Manufacturer.html", context)
 
 def Review(request):
-		context = { "imageURL":str(),"phoneName":str(),"scores":[],	"vergePros":list(str()),	"vergeCons":list(str()),	"cnetPros":list(str()),"wiredPros":list(str()),	"wiredCons":list(str()),	"pcMagPros":list(str()),	"pcMagCons":list(str()),	"cnetSubScores":dict()} # scores: List of ("name",fltScore) tuples, 
-		return render(request, "CellCheck/Review.html", context)
+	# scores: List of ("name",fltScore) pairs
+	context = { 
+				"imageURL":str(),
+				"phoneName":str(),
+				"scores":[],
+				"vergePros":list(str()),
+				"vergeCons":list(str()),
+				"cnetPros":list(str()),
+				"wiredPros":list(str()),
+				"wiredCons":list(str()),
+				"pcMagPros":list(str()),
+				"pcMagCons":list(str()),
+				"cnetSubScores":dict()
+				}
+	return render(request, "CellCheck/Review.html", context)
