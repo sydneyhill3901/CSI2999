7ec02935653fbed51a702910a9b59e409994bb29
Mike <mooremk95@gmail.com>
2020-03-03 22:31:17 -0500

    SQL Models Created, Admin Panel Updated
    The SQL model classes have been created. SQLite3 db can be setup locally by running the following commands in the django project folder (has manage.py in it)
    python manage.py makemigrations CellCheck
    python manage.py migrate
    
    Admin panel set up to edit SQL tables (works like PHPMyAdmin). To create a local admin account run the following command in main django project directory
    python manage.py createsuperuser

--- CSI2999/CellCheck/admin.py
+++ CSI2999/CellCheck/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Phone, Site, Rating, ConList, ProList, CNETDetailedScore
 
 # Register your models here.
+
+admin.site.register(Phone)
+admin.site.register(Site)
+admin.site.register(Rating)
+admin.site.register(ConList)
+admin.site.register(ProList)
+admin.site.register(CNETDetailedScore)
+
--- CSI2999/CellCheck/models.py
+++ CSI2999/CellCheck/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
-# Create your models here.
+"""
+Notes:
+	- Each model class (ie. child class of models.Model) corresponds to one sql table
+ 	- In python a class is defined as a child of a parent by putting the parent in the
+ 	  parentheses after the class name is defined
+	- by default model classes generate an id column w/ Not Null and Primary Key settings
+""" 
+
+class Phone(models.Model):
+	PhoneName = models.CharField(max_length = 40)
+	# I am gonna set this to 160 for now, might need to make larger
+	CnetURL = models.CharField(max_length = 160) 
+	WiredURL = models.CharField(max_length = 160) 
+	PCMagURL = models.CharField(max_length = 160) 
+	VergeURL = models.CharField(max_length = 160) 
+	# A date field so we know when the phone was added to the database
+	DataAdded = models.DateField()
+
+class Site(models.Model):
+	# Having this as a table means we only have to store foreign keys
+	# in rating table entries rather than redundant strings, saving space
+	# also more extensible for the "future"
+	Name = models.CharField(max_length = 40)
+
+class Rating(models.Model):
+	# Need to store reviews as floats b/c some sites scores are to 1st decimal point
+	Rating = models.DecimalField(max_digits=2, decimal_places=1) 
+	# This is how you make a foreign key in django. Note the cascading delete
+	PhoneID = models.ForeignKey(Phone, on_delete=models.CASCADE)
+	SiteID = models.ForeignKey(Site, on_delete=models.CASCADE)
+
+class ProList(models.Model):
+	PhoneID = models.ForeignKey(Phone, on_delete=models.CASCADE)
+	SiteID = models.ForeignKey(Site, on_delete=models.CASCADE)
+	Pros = models.CharField(max_length = 200)
+
+class ConList(models.Model):
+	PhoneID = models.ForeignKey(Phone, on_delete=models.CASCADE)
+	SiteID = models.ForeignKey(Site, on_delete=models.CASCADE)
+	Cons = models.CharField(max_length = 200)
+
+class CNETDetailedScore(models.Model):
+	phoneID = models.ForeignKey(Phone, on_delete=models.CASCADE)
+	Design = models.PositiveSmallIntegerField()
+	Features = models.PositiveSmallIntegerField()
+	Performance = models.PositiveSmallIntegerField()
+	Camera = models.PositiveSmallIntegerField()
+	Battery = models.PositiveSmallIntegerField()
+	
