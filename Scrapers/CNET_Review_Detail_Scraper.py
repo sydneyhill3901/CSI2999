584694c46df830080607b725a0a65169bfe3ee24
Andrew <hupfel@oakland.edu>
2020-03-13 18:26:11 -0400

    CNET Score Breakdown and Pros Cons Update
    Update to code that takes the score breakdown and pros and cons and places them in the database.

--- Scrapers/CNET_Review_Detail_Scraper.py
+++ Scrapers/CNET_Review_Detail_Scraper.py
@@ -2,6 +2,7 @@ import csv
 import requests
 from bs4 import BeautifulSoup
 import time
+import sqlite3
 
 
 phoneList = []
@@ -48,6 +49,8 @@ def search_goodreviews(phone_name, websource):
     phonereview = websource.find_all("div", {"class": "c-reviewCard_chunk"})
     phonereviewslist = []
     phonereviewsentimentlist = []
+    GoodReview = ""
+    BadReview = ""
     for i in range(len(phonereview)):
         if "Like" in phonereview[i].text:
             phone_statements = phonereview[i].find_all("span", {"class": "c-reviewCard_listText"})
@@ -55,7 +58,12 @@ def search_goodreviews(phone_name, websource):
                 phonereviewslist.append(statements.text)
                 phonereviewsentimentlist.append(phonereview[i].h3.text)
     for i in range(len(phonereviewslist)):
-        phoneGoodBadBottomLineList.append([phone_name, phonereviewslist[i], phonereviewsentimentlist[i]])
+        if phonereviewsentimentlist[i] == "Don't Like":
+            BadReview = BadReview + "\n" + phonereviewslist[i]
+        else:
+            GoodReview = GoodReview + "\n" + phonereviewslist[i]
+    phoneGoodBadBottomLineList.append([phone_name, GoodReview.strip(), "Like"])
+    phoneGoodBadBottomLineList.append([phone_name, BadReview.strip(), "Don't Like"])
 
 
 def search_scorebreakdown_page_type_2(phone_name, websource):
@@ -100,25 +108,112 @@ def read_csv():
             phoneList.append(row)  # Adding the csv to a list to search each page
 
 
+# Source: https://docs.python.org/3/library/sqlite3.html
+def PlaceScoresInDatabase(phone, header, rating, conn, c):
+    c.execute("SELECT id FROM CellCheck_Phone WHERE PhoneName=?", (phone.strip().lower(),))
+    phoneid = c.fetchone()[0]
+    c.execute("SELECT count(*) FROM CellCheck_CNETDetailedScore WHERE Phone_id = ?", (phoneid,))
+    exists = c.fetchone()[0]
+    if exists == 0:
+        c.execute("INSERT INTO CellCheck_CNETDetailedScore (Phone_id, Design, Features, Performance, Camera, Battery)"
+                  " values (?, ?, ?, ?, ?, ?)",
+                  (phoneid, rating, "", "", "", ""))
+        conn.commit()
+    elif header == "Design":
+        c.execute("UPDATE CellCheck_CNETDetailedScore SET Design = ? WHERE Phone_id = ?", (rating, phoneid,))
+        conn.commit()
+    elif header == "Features":
+        c.execute("UPDATE CellCheck_CNETDetailedScore SET Features = ? WHERE Phone_id = ?", (rating, phoneid,))
+        conn.commit()
+    elif header == "Performance":
+        c.execute("UPDATE CellCheck_CNETDetailedScore SET Performance = ? WHERE Phone_id = ?", (rating, phoneid,))
+        conn.commit()
+    elif header == "Camera":
+        c.execute("UPDATE CellCheck_CNETDetailedScore SET Camera = ? WHERE Phone_id = ?", (rating, phoneid,))
+        conn.commit()
+    elif header == "Battery":
+        c.execute("UPDATE CellCheck_CNETDetailedScore SET Battery = ? WHERE Phone_id = ?", (rating, phoneid,))
+        conn.commit()
+
+
+# Source: https://docs.python.org/3/library/sqlite3.html
+def UpdateProDatabase(phone, description, conn, c):
+    c.execute("SELECT id FROM CellCheck_Phone WHERE PhoneName=?", (phone.strip().lower(),))
+    phoneid = c.fetchone()[0]
+    c.execute("SELECT * FROM CellCheck_Site WHERE SiteName = 'CNET'")
+    siteid = c.fetchone()[0]
+    c.execute("SELECT count(*) FROM CellCheck_ProList WHERE Phone_id = ? AND Site_id = ?",
+              (phoneid, siteid))
+    exists = c.fetchone()[0]
+    if exists == 0:
+        c.execute("INSERT INTO CellCheck_ProList (Phone_id, Site_id, Pros)"
+                  " values (?, ?, ?)",
+                  (phoneid, siteid, description.strip()))
+        conn.commit()
+
+
+# Source: https://docs.python.org/3/library/sqlite3.html
+def UpdateConDatabase(phone, description, conn, c):
+    c.execute("SELECT id FROM CellCheck_Phone WHERE PhoneName=?", (phone.strip().lower(),))
+    phoneid = c.fetchone()[0]
+    c.execute("SELECT * FROM CellCheck_Site WHERE SiteName = 'CNET'")
+    siteid = c.fetchone()[0]
+    c.execute("SELECT count(*) FROM CellCheck_ConList WHERE Phone_id = ? AND Site_id = ?",
+              (phoneid, siteid))
+    exists = c.fetchone()[0]
+    if exists == 0:
+        c.execute("INSERT INTO CellCheck_ConList (Phone_id, Site_id, Cons)"
+                  " values (?, ?, ?)",
+                  (phoneid, siteid, description.strip()))
+        conn.commit()
+
+
 def main():
     read_csv()  # Start main by reading the csv and putting items into a list
     for phones in range(18):
-        page_text = get_webpage(phoneList[phones][1])  # Gets the page text to grab the info from
-        if page_text.find_all("span", {"class": "c-reviewPostcap_rateTitle"}):
-            search_scorebreakdown(phoneList[phones][0], page_text)
-        elif page_text.find_all("div", {"class": "categoryWrap"}):
-            search_scorebreakdown_page_type_2(phoneList[phones][0], page_text)
-        if page_text.find_all("div", {"class": "c-reviewCard_chunk"}):
-            search_goodreviews(phoneList[phones][0], page_text)
-        elif page_text.find_all("p", {"class": "theGood"}):
-            search_theGood(phoneList[phones][0], page_text)
-            search_theBad(phoneList[phones][0], page_text)
-            search_theBottomLine(phoneList[phones][0], page_text)
+        try:
+            page_text = get_webpage(phoneList[phones][1])  # Gets the page text to grab the info from
+            if page_text.find_all("span", {"class": "c-reviewPostcap_rateTitle"}):
+                search_scorebreakdown(phoneList[phones][0], page_text)
+            elif page_text.find_all("div", {"class": "categoryWrap"}):
+                search_scorebreakdown_page_type_2(phoneList[phones][0], page_text)
+            if page_text.find_all("div", {"class": "c-reviewCard_chunk"}):
+                search_goodreviews(phoneList[phones][0], page_text)
+            elif page_text.find_all("p", {"class": "theGood"}):
+                search_theGood(phoneList[phones][0], page_text)
+                search_theBad(phoneList[phones][0], page_text)
+                search_theBottomLine(phoneList[phones][0], page_text)
+        except Exception as e:
+            print(f"Error syncing to Database \n{e}")
         time.sleep(10)
     CreateScoreBreakdownCSV()
     CreateGoodBadCSV()
     # print(scoreBreakdownList)
-    print(phoneGoodBadBottomLineList)
+    # print(phoneGoodBadBottomLineList)
+
+    # # Add info into database
+    # conn = sqlite3.connect(r'C:\Users\Andrew\Documents\CSI2999\CSI2999\CSI2999\db.sqlite3')
+    conn = sqlite3.connect("../CSI2999/db.sqlite3")
+    c = conn.cursor()
+    for i in scoreBreakdownList:
+        phone = i[0]
+        scoreHeader = i[1]
+        scoreRating = i[2]
+        PlaceScoresInDatabase(phone, scoreHeader, scoreRating, conn, c)
+    for i in phoneGoodBadBottomLineList:
+        phone = i[0]
+        description = i[1]
+        sentiment = i[2]
+        if sentiment == 'Like':
+            UpdateProDatabase(phone, description, conn, c)
+        elif sentiment == 'Good':
+            UpdateProDatabase(phone, description, conn, c)
+        elif sentiment == "Don't Like":
+            UpdateConDatabase(phone, description, conn, c)
+        elif sentiment == "Bad":
+            UpdateConDatabase(phone, description, conn, c)
+         # PlaceSentimentInDatabase(phone, description, sentiment, conn, c)
+    conn.close()
 
 
 # Start with the main function of the code
