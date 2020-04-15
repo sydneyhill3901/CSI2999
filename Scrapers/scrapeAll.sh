0fb8e02a77662e72f807387d749eb57420e60275
Mike <mooremk95@gmail.com>
2020-04-15 01:20:43 -0400

    Review Template set for User Reviews, fixed UserReview div
    The UserReview div now has the template tags in place for proper rendering of the data once the view provides it. Fixed the misaligned Best Buy and Sprint lists, due to the sprint list tag being mispelled <u1>

---
+++ Scrapers/scrapeAll.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#python vergeScraper.py
+#python pcmagReviewsScraper.py
+#python wiredReviewsScraper.py
+#python bestBuyReviewScraper.py
+#python sprintReviewScraper.py
+python CNET_Phone_Rating_Url_Scraper.py
+python CNET_Review_Detail_Scraper.py
